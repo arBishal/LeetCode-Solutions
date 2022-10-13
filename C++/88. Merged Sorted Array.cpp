23a0ebe8a2b07f4c5d4c09159f226fcfe41c2b24
arBishal <arbishal.319@gmail.com>
2022-10-14 03:43:01 +0600

    merged sorted array new

--- C++/88. Merged Sorted Array.cpp
+++ C++/88. Merged Sorted Array.cpp
@@ -8,3 +8,50 @@ public:
         sort(nums1.begin(), nums1.end());
     }
 };
+
+///O(m+n) complexity
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int index=m+n-1;
+        m--;
+        n--;
+        while(m>=0 and n>=0)
+        {
+            if(nums1[m]>nums2[n])
+            {
+                nums1[index]=nums1[m];
+                index--;
+                m--;
+            }
+            else if(nums2[n]>nums1[m])
+            {
+                nums1[index]=nums2[n];
+                index--;
+                n--;
+            }
+            else
+            {
+                nums1[index]=nums1[m];
+                index--;
+                m--;
+                nums1[index]=nums2[n];
+                index--;
+                n--;
+            }
+        }
+        while(m>=0)
+        {
+            nums1[index]=nums1[m];
+            index--;
+            m--;
+        }
+        while(n>=0)
+        {
+            nums1[index]=nums2[n];
+            index--;
+            n--;
+        }
+    }
+};
