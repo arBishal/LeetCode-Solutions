996551acd6c2d5045b11ae6d20df6d09479ab8a2
arBishal <arbishal.319@gmail.com>
2025-01-03 18:56:42 +0600

    455

---
+++ C++/455. Assign Cookies.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int count = 0, i = 0, j = 0;
+
+        while(i < g.size() and j < s.size())
+        {
+            if(g[i] <= s[j])
+            {
+                count++;
+                i++;
+                j++;
+            }
+            else
+            {
+                j++;
+            }
+        }
+
+        return count;
+    }
+};
+
